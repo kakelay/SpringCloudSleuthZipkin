e716f701822b7f7088ebe85257e4829c1c13dc72
kakelay <kakelay18052002@gmail.com>
2025-03-06 00:19:12 +0700

    update code

--- src/main/java/com/rean/repository/CourseRepository.java
+++ src/main/java/com/rean/repository/CourseRepository.java
@@ -3,21 +3,22 @@ package com.rean.repository;
 import com.rean.model.Course;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
 import java.awt.print.Pageable;
 import java.util.Optional;
 
 public interface CourseRepository  extends JpaRepository<Course,Long> {
 
+    //  Find by ID
     Optional<Course> findFirstById(Long id);
 
+    //  Find All by Pageable
     Page<Course> findAllBy(Pageable pageable);
 
+    //  Find Can be any text sting
     Page<Course> findAllByNameContainingIgnoreCase(String name, Pageable pageable);
 
     Page<Course>findAllByPrice( double price ,  Pageable pageable);
 
-    Page<Course> findAllByNameIsContainingIgnoreCaseAndPrice( String name, double price, Pageable pageable);
+    Page<Course> findAllByNameContainingIgnoreCaseAndPrice( String name, double price, Pageable pageable);
 
  }
